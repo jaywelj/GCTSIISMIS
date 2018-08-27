80bff1792999a7e86cc5e84675bab808cb53dc3c
jaywelj <jaywelj@gmail.com>
2018-08-27 11:05:41 +0800

    270818JaywelBranch

--- client2/logout.php
+++ client2/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
-echo "<script> alert('You are now logging out'); window.location.replace('index.php');</script>";
-// header("location: index.php");
+unset($_SESSION['sessionStudentAccountNumber']);
+unset($_SESSION['sessionStudentAccountPassword']);
+echo "<script> alert('You are now logging out');</script>";
+header("location: index.php");
 ?>
